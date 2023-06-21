33cc67c7ffcf756d102fefd4bf22e734d7b328f4
Ukeme <ukeme@localhost.localdomain>
2023-06-21 07:37:01 +0100

    working on about section

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
+const navItems = document.querySelectorAll('.nav-item');
+const navOverlay = document.querySelector('.nav-overlay');
 
 
 
-hamburgerMenu.addEventListener('click', function() {
-	this.classList.toggle('active');
-});
+hamburgerMenu.addEventListener('click', navEvent);
+
+navOverlay.addEventListener('click', navEvent);
+
+function navEvent() {
+	hamburgerMenu.classList.toggle('active');
+	navMenu.classList.toggle('active');
+	navOverlay.classList.toggle('active');
+};
+
+navItems.forEach(function(navItem, index) {
+	navItem.style.transitionDelay = `${0.12 + (index * 0.1)}s`;
+})
