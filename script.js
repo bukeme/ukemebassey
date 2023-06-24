687ce71d06b1e47557751b41b6c16e6c7fdbcb3d
Ukeme Bassey <basseyukemme@gmail.com>
2023-06-24 10:47:22 +0100

    working on some sections animation

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
 const navItems = document.querySelectorAll('.nav-item');
 const navOverlay = document.querySelector('.nav-overlay');
+const aboutmeImg = document.querySelector('.about-me__img-con');
+const aboutmeText = document.querySelector('.about-me__text');
+const skills = document.querySelectorAll('.skill');
+const projectCards = document.querySelectorAll('.project-card');
+const navLinks = document.querySelectorAll('.nav-item a');
+
+let animationItemList = [aboutmeImg, aboutmeText];
+animationItemList = animationItemList.concat(Array.from(skills), Array.from(projectCards));
+console.log(animationItemList);
 
 
 
 hamburgerMenu.addEventListener('click', navEvent);
 
 navOverlay.addEventListener('click', navEvent);
+
+navLinks.forEach((navLink) => navLink.addEventListener('click', navEvent));
 
 function navEvent() {
 	hamburgerMenu.classList.toggle('active');
@@ -17,4 +28,18 @@ function navEvent() {
 
 navItems.forEach(function(navItem, index) {
 	navItem.style.transitionDelay = `${0.11 + (index * 0.05)}s`;
+});
+
+function animateContent(itemList) {
+	itemList.forEach(function(item) {
+		let triggerLine = window.innerHeight - +item.dataset.line;
+		let itemLine = item.getBoundingClientRect().top;
+		if (itemLine <= triggerLine) {
+			item.classList.add('animate');
+		}
+	});
+};
+
+window.addEventListener('scroll', function() {
+	animateContent(animationItemList);
 })
